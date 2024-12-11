da7e82aa502850d1f735dfb87bbe58a3434b8042
Marcos Rene <renemoura789@gmail.com>
2024-12-11 11:50:00 -0300

    feat: add welcome component

---
+++ mobile/src/components/welcome/index.tsx
@@ -0,0 +1,14 @@
+import { Image, Text, View } from "react-native";
+import { styles } from "./styles";
+
+export function Welcome() {
+  return (
+    <View>
+      <Image source={require("@/assets/logo.png")} style={styles.logo} />
+      <Text style={styles.title}>Welcome to Nearby!</Text>
+      <Text style={styles.subtitle}>
+        Get advantage coupons to use in your favorite establishments.
+      </Text>
+    </View>
+  );
+}
